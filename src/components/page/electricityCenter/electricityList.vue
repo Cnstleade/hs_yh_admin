416434b3e5f91bdbd184b6d58667a67493053c81
chb921029cj <413055526@qq.com>
2018-06-29 10:56:46 +0800

    c8

--- src/components/page/electricityCenter/electricityList.vue
+++ src/components/page/electricityCenter/electricityList.vue
@@ -44,97 +44,23 @@
             class="m20"
             v-loading="loading"
           >
-            <el-table-column prop="id" label="订单号" align="center"  sortable></el-table-column>
-            <el-table-column prop="userName" label="姓名" align="center" ></el-table-column>
-            <el-table-column prop="mobile" label="手机号" align="center" ></el-table-column>
-            <el-table-column prop="idNo" label="身份证号" align="center" width="200"></el-table-column>
-            <el-table-column prop="applyTime" label="申请时间" align="center" width="180" sortable>
-                <template slot-scope="scope">
-                    {{scope.row.applyTime|dateServer}}
-                </template>
-            </el-table-column>
-            <el-table-column prop="approveAmt" label="审核金额" align="center" sortable ></el-table-column>
-            <el-table-column prop="collectorUserName" label="催收员" align="center"  >
-                <template slot-scope="scope">
-                    {{scope.row.collectorUserName?scope.row.collectorUserName:'暂无催收员'}}
-                </template>
-            </el-table-column>            
-            <el-table-column type="expand" label="更多详情" width="80" >
-              <template slot-scope="props" >
-                <el-alert
-                  title="提现情况"
-                  type="success"
-                  :closable="false"
-                  center
-                  >
-                </el-alert>
-                <el-table
-                  :data="props.row.detail.withdraws?props.row.detail.withdraws:[]"
-                  border 
-                  style="width: 100%"
-                  >
-                      <el-table-column prop="id" label="id" align="center" sortable></el-table-column>
-                      <el-table-column prop="withdrawMoney" label="提现金额" align="center" sortable></el-table-column>
-                      <el-table-column prop="borrowTime" label="提现时间" align="center" sortable>
+                      <el-table-column prop="loanOrderId" label="订单ID" align="center" ></el-table-column>
+                      <el-table-column prop="companyId" label="机构ID" align="center" ></el-table-column>
+                      <el-table-column prop="custUserId" label="用户ID" align="center" ></el-table-column>
+                      <el-table-column prop="custUserMobile" label="客户电话" align="center" ></el-table-column>
+                      <el-table-column prop="type" label="进入电销类型" align="center" ></el-table-column>
+                      <el-table-column prop="companyName" label="机构名" align="center" ></el-table-column>
+                      <el-table-column prop="companyName" label="客户姓名" align="center" ></el-table-column>
+                      <el-table-column prop="companyName" label="所剩金额" align="center" ></el-table-column>
+                    <el-table-column prop="cz"  align="center" label="操作"   >
                         <template slot-scope="scope">
-                            {{scope.row.borrowTime|dateServer}}
-                        </template>                          
-                      </el-table-column>
-                      <el-table-column prop="status" label="是否逾期" align="center" 
-                         :filters="[{ value: 0, text: '放款中 ' }, { value: 1, text: '放款成功' }, { value: 2, text: '逾期' }, { value: 3, text: '还款成功' }, { value: 4, text: '放款失败' }, { value: 5, text: '还款中' }, { value: 6, text: '还款失败' }]"
-                         :filter-method="filterStauts"                      
-                      >
-                        <template slot-scope="scope">
-                            <el-tag
-                                :type="scope.row.status===0?'':scope.row.status===1?'success':scope.row.status===2?'danger':scope.row.status===4?'success':scope.row.status===4?'info':scope.row.status===5?'':'warning'"
-                            >{{scope.row.status===0?'放款中':scope.row.status===1?'放款成功':scope.row.status===2?'逾期':scope.row.status===3?'还款成功':scope.row.status===4?'放款失败':scope.row.status===5?'还款中':'还款失败'}}</el-tag>
-                        </template>                         
-                      </el-table-column>
-                      <el-table-column prop="overdue.overdueDay" label="逾期天数" align="center" sortable></el-table-column>
-                      <el-table-column prop="overdue.lateFee" label="违约金" align="center" sortable></el-table-column>
-                </el-table>
-                <el-alert
-                  title="催收情况"
-                  type="success"
-                  :closable="false"
-                  center
-                  class="m20"
-                  >
-                </el-alert>                
-                <el-table
-                  :data="props.row.detail.loanCollectionRecords?props.row.detail.loanCollectionRecords:[]"
-                  border 
-                  style="width: 100%"
-                  >
-                      <el-table-column prop="id" label="id" align="center" ></el-table-column>
-                      <el-table-column prop="operatorName" label="催收员姓名" align="center" ></el-table-column>
-                      <el-table-column prop="remark" label="描述（备注）" align="center" ></el-table-column>
-                      <el-table-column prop="result" label="结果" align="center" >
-                        <template slot-scope="scope">
-                            <el-tag
-                                :type="scope.row.status===1?'':scope.row.status===2?'danger':'success'"
-                            >{{scope.row.status===1?'成功':scope.row.status===2?'失败':'进行中 '}}</el-tag>
-                        </template>                         
-                      </el-table-column>
-                      <el-table-column prop="type" label="催收方式" align="center" >
-                        <template slot-scope="scope">
-                            <el-tag
-                                :type="scope.row.status===1?'':scope.row.status===2?'danger':'success'"
-                            >{{scope.row.status===1?'电话':scope.row.status===2?'短信':'其他 '}}</el-tag>
-                        </template>                         
-                      </el-table-column>                      
-                </el-table>                
-              </template>
-            </el-table-column> 
-                <el-table-column prop="cz"  align="center" label="操作"   >
-                    <template slot-scope="scope">
-                    <el-button
-                        size="mini"
-                        type="success"
-                        @click="handleAllocation(scope.$index, scope.row)"
-                       >分配</el-button>
-                    </template> 
-                </el-table-column>            
+                        <el-button
+                            size="mini"
+                            type="success"
+                            @click="handleAllocation(scope.$index, scope.row)"
+                           >分配</el-button>
+                        </template> 
+                    </el-table-column>   
         </el-table>  
         <el-row class="m20" v-if="total>0">
             <el-button type="primary" style="float:left" @click="handelConfigAll" :disabled="multipleSelection.length==0">批量分配</el-button>
